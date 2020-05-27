b52523ddc1bc0fac6f40d01cf0291cd51ffa3705
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-05-27 15:03:58 -0700

    Additional CSS properties

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import Logo from "./img/logo-white.png";
 
 const App = () => {
   return (
+
     <header className="header">
       <div className="logo-box">
         <img src={Logo} className="logo" alt="Logo" />
       </div>
-      <h1 className="heading-primary">
-        <span className="heading-primary-main">Niocraft</span>
-        <span className="heading-primary-sub">is where life happens</span>
-      </h1>
+      <div className="text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary-main">Niocraft</span>
+          <span className="heading-primary-sub">is where life happens</span>
+        </h1>
+      </div>
+
     </header>
+
+
   );
 };
 
