30840a4dbf4f1ed56de8c500dc96a1ab98408cc2
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-05-28 13:11:51 -0700

    Organizing files between react & scss

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import About from '../../sections/About';
-import Features from "../../sections/Features";
-import Tours from '../../sections/Tours';
-import Stories from '../../sections/Stories';
-import Book from '../../sections/Book';
+import About from './sections/About';
+import Features from "./sections/Features";
+import Tours from './sections/Tours';
+import Stories from './sections/Stories';
+import Book from './sections/Book';
 
 const Home = () => {
     return (
