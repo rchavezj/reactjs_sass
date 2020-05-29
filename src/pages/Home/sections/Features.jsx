ef85d29939170f39a693e13e15864aa0d7f3e16d
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-05-29 09:14:14 -0700

    Added  new prop for featureBox called journey

--- src/pages/Home/sections/Features.jsx
+++ src/pages/Home/sections/Features.jsx
@@ -7,10 +7,34 @@ const Features = () => {
     return (
         <section className="section-features">
             <div className="row">
-                <FeatureBox title={`Explore the world`}/>
-                <FeatureBox title={`Meet nature`} />
-                <FeatureBox title={`Find your way`} />
-                <FeatureBox title={`Live a healthier life`}/>
+                <FeatureBox
+                    title={`Explore the world`}
+                    journey={`
+                        Lorem ipsum dolor sit amet consectetur 
+                        adipisicing elit. Aperiam, ipsum sapiente aspernatur.`
+                    }
+                />
+                <FeatureBox
+                    title={`Meet nature`}
+                    journey={`
+                        Lorem ipsum dolor sit amet consectetur 
+                        adipisicing elit. Aperiam, ipsum sapiente aspernatur.`
+                    }
+                />
+                <FeatureBox
+                    title={`Find your way`}
+                    journey={`
+                        Lorem ipsum dolor sit amet consectetur 
+                        adipisicing elit. Aperiam, ipsum sapiente aspernatur.`
+                    }
+                />
+                <FeatureBox
+                    title={`Live a healthier life`}
+                    journey={`
+                        Lorem ipsum dolor sit amet consectetur 
+                        adipisicing elit. Aperiam, ipsum sapiente aspernatur.`
+                    }
+                />
             </div>
         </section>
     )
